26e1727a504b2b15e55aff01930e643b14f6124c
Dan Bikle <bikle@bikle.com>
2011-10-18 23:42:00 +0100

    features/r10.feature

--- features/r10.feature
+++ features/r10.feature
@@ -1,9 +1,29 @@
 Feature: See r10 index action
   I want to see r10 index action.
 
-  @javascript
+  @javascriptx
+  Scenario: Add Rails-partial named lhs links
+    Given good_luck
+    When I visit "/"
+    Then I should see "layouts/_lhs_links.html.slim" in a comment
+    And I_should_see_lhs_links
+
+  Scenario: Go to slash
+    Given good_luck
+    When I visit "/"
+    Then I should see "About"
+    And I should see "layouts/application.html.haml" in a comment
+    And I should see "views/r10/index.html.slim" in a comment
+
+  Scenario: Go to r10 index
+    Given good_luck
+    When I visit "/r10/index"
+    Then I should see "About"
+    And I should see "layouts/application.html.haml" in a comment
+    And I should see "layouts/_lhs_links.html.slim" in a comment
+    And I should see "views/r10/index.html.slim" in a comment
+
   Scenario: Homepage
     Given I am on the homepage
-    And debug
     Then I should see "Bot4.us"
 
--- features/step_definitions/r10.rb
+++ features/step_definitions/r10.rb
@@ -1,5 +1,36 @@
 # r10.rb
 
+Then /^I_should_see_lhs_links$/ do
+  ndoc = Nokogiri::HTML page.html
+  my_links = ndoc.css("a")
+  my_links.size.should > 1
+  ml_html_a = my_links.map{|ml|ml.inner_html}
+  (ml_html_a.include? "About").should == true
+  # Check for a few links, not all.
+  ["About","Blog","Books","Contact","Forum","FAQ","Glossary"].each{|lnk| (ml_html_a.include? lnk).should == true}
+  href_a = my_links.map{|l| l.attributes["href"].value}.sort.uniq
+  ["/about", "/blog", "/books", "/contact", "/disclaimer", "/faq"].each{|href| (href_a.include? href).should == true}
+end
+
+Given /^good_luck$/ do
+  assert true
+end
+
+
+When /^I visit "([^"]*)"$/ do |path|
+  visit path
+end
+
+Then /^(?:|I )should see "([^"]*)" in a comment$/ do |text|
+  assert false unless page.html =~ Regexp.new(Regexp.escape(text))
+end
+
+Then /^smtp$/ do
+  # smtp is show me the page
+  save_and_open_page
+  assert true
+end
+
 Given /^I am on the homepage$/ do
   visit '/'
 end
